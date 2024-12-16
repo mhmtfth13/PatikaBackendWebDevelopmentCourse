6f9c99885ccd935cbf68e8b00cecd9a512015220
mhmtfth13 <mhmtfthaydin@gmail.com>
2024-12-16 18:47:03 +0300

    Added IlkUygulamam File. (Classical C# hello world console app.)

---
+++ WEEK1/IlkUygulamam/IlkUygulamam/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mehmet Fatih Aydin");
+            Console.WriteLine("Patika Bootcampine katilma amacim, API yapilarini kullanarak kendime ait web tabanli karmasik sistemler yazmak istiyorum.\nKendime ait bir crypto bot uygulamalari yazmak istiyorum.");
+            Console.ReadKey();
+        }
+
+    }
+}

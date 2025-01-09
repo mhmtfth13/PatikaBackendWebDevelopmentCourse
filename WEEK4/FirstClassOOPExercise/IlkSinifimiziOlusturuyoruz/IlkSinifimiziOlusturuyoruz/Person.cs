e7ffe3bae8d10f5df174dba3483e1dc2b18d09f9
Mehmet Fatih Aydın <116929673+mhmtfth13@users.noreply.github.com>
2025-01-09 06:29:20 +0300

    Update Person.cs

--- WEEK4/FirstClassOOPExercise/IlkSinifimiziOlusturuyoruz/IlkSinifimiziOlusturuyoruz/Person.cs
+++ WEEK4/FirstClassOOPExercise/IlkSinifimiziOlusturuyoruz/IlkSinifimiziOlusturuyoruz/Person.cs
@@ -9,12 +9,12 @@ namespace IlkSinifimiziOlusturuyoruz
 {
     public class Person
     {
-        public string Ad { get; set; }
-        public string Soyad { get; set; }
-        public DateTime DogumTarihi { get; set; }
+        public string Ad { get; set; } // Ad adinda bir Person sinifina bir property ozellik olusturuyoruz.
+        public string Soyad { get; set; } // Soyad adinda bir Person sinifina bir property ozellik olusturuyoruz.
+        public DateTime DogumTarihi { get; set; } // DogumTarihi adinda bir Person sinifina bir property ozellik olusturuyoruz.
 
 
-        public void BilgiGetir ()
+        public void BilgiGetir () // Person classina ait bir BilgiGetir methodu olusturuyoruz. Burada olusturulan nesnenin adini, soyadini ve dogum tarihini ekrana yaziyoruz.
         {
             Console.WriteLine($"Isminiz : {Ad}\nSoyisminiz:{Soyad}\nDogum Tarihniz:{DogumTarihi}");
         }

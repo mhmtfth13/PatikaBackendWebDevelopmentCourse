edcb059c37777c35ff102ff9a2b2bd0b3cea251b
mhmtfth13 <mhmtfthaydin@gmail.com>
2025-01-09 06:20:40 +0300

    added files

---
+++ WEEK4/FirstClassOOPExercise/IlkSinifimiziOlusturuyoruz/IlkSinifimiziOlusturuyoruz/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkSinifimiziOlusturuyoruz
+{
+    public class Person
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+
+        public void BilgiGetir ()
+        {
+            Console.WriteLine($"Isminiz : {Ad}\nSoyisminiz:{Soyad}\nDogum Tarihniz:{DogumTarihi}");
+        }
+    }
+}
---
+++ WEEK4/FirstClassOOPExercise/IlkSinifimiziOlusturuyoruz/IlkSinifimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,36 @@
+﻿using IlkSinifimiziOlusturuyoruz;
+using System;
+
+namespace PatikaWeek4.IlkSinifOlusturuyoruzExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Ogretmen1 = new Person();
+            Person Ogrenci1 = new Person
+            {
+                Ad = "Mehmet",
+                Soyad ="Aydin",
+                DogumTarihi = new DateTime(2000,02,20)
+            };
+            Person Ogrenci2 = new Person
+            {
+                Ad= "Salih",
+                Soyad= "Aydin",
+                DogumTarihi = new DateTime(2010,01,18)
+            };
+
+            Ogretmen1.Ad = "Ahmet";
+
+            Ogretmen1.Soyad = "Sonuc";
+            Ogretmen1.DogumTarihi = new DateTime(2000,1,1);
+
+            Ogretmen1.BilgiGetir();
+            Ogrenci1.BilgiGetir();
+            Ogrenci2.BilgiGetir();
+            Console.ReadKey();
+            
+        }
+    }
+}

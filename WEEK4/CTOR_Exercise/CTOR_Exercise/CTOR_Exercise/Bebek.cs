c3e42796291acea8e8808657528ed4ef32f95a5a
mhmtfth13 <mhmtfthaydin@gmail.com>
2025-01-10 05:31:20 +0300

    added files

---
+++ WEEK4/CTOR_Exercise/CTOR_Exercise/CTOR_Exercise/Bebek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTOR_Exercise
+{
+    internal class Bebek
+    {
+
+        public Bebek()
+        {
+            Console.WriteLine("Ingaaaa");
+            _birthDate = DateTime.Now.ToString();
+        }
+
+        public Bebek(string name,string lastname)
+        {
+            _name = name;
+            _lastname = lastname;
+            Console.WriteLine("Ingaaaa");
+            _birthDate = DateTime.Now.ToString();
+        }
+        public string _birthDate;
+        public string _name;
+        public string _lastname;
+
+
+        
+
+    }
+}
---
+++ WEEK4/CTOR_Exercise/CTOR_Exercise/CTOR_Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using CTOR_Exercise;
+using System;
+
+namespace PatikaWeek4.CTOR_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebek bebek1 = new Bebek();// Default CTOR kullandik.
+            Console.WriteLine($"Bebek1: Dogum Tarihi:{bebek1._birthDate}");
+            Bebek bebek2 = new Bebek("Ali","Veli"); // Burada method overloading oldu 2. yaptigimiz CTOR kullandik.
+            Console.WriteLine($"Bebek2: Bebek2 Adi:{bebek2._name}, Bebek2 Soyadi:{bebek2._lastname}, Bebek2 Dogum Tarihi:{bebek2._birthDate}");
+            Console.ReadKey();
+        }
+    }
+}

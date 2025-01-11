b9eb463552433c87fb7fa8e16882b38e18e0a7b5
mhmtfth13 <mhmtfthaydin@gmail.com>
2025-01-11 22:40:30 +0300

    added files

---
+++ WEEK4/PatikaOOPLibraryExercise/PatikaOOPLibraryExercise/Kitap.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOOPLibraryExercise
+{
+    internal class Kitap // Burada Kitap adinda bir class (sinif) olusturduk.
+    {
+        private string _bookName; // // Burada encapsulation seklinde degisken tanimlama yapildi.
+        private string _writerName; // // Burada encapsulation seklinde degisken tanimlama yapildi.
+        private string _writerSurname; // // Burada encapsulation seklinde degisken tanimlama yapildi.
+        private int _pageNumber; // // Burada encapsulation seklinde degisken tanimlama yapildi.
+        private string _publisherName; // // Burada encapsulation seklinde degisken tanimlama yapildi.
+        private DateTime _signTime; // // Burada encapsulation seklinde degisken tanimlama yapildi.
+
+        public Kitap() // Burada CTOR yani parametresiz bir constructor olusturuldu. Temel mantik bir Kitap classi icin nesne olusturuldugunda buraya otomatik olarak ugrayacak ve altinda yapmis oldugun datetime degerini en olursa olsun alacak.
+        {
+            _signTime = DateTime.Now;
+        }
+        public Kitap(string bookName, string writerName, int pageNumber, string publisher)// Burada parametre alan bir ctor  constructor tanimlanmistir. Burada kisiden kitap adi, yazar adi, kitap sayfasi ve kitap yayinevini bekledigimizi belirtiyoruz. Burada bir override islemi de oluyor.
+        {
+            _bookName = bookName; // Burada _bookName seklinde private olarak tanimladigimiz degiskenleri public olarak eristigimiz Kitap nesnesinin propertieslerine eristiriyoruz ve atama yapiyoruz..
+            _writerName = writerName;
+            _pageNumber = pageNumber;
+            _publisherName = publisher;
+            _signTime= DateTime.Now;
+        }
+
+        public string Name // Burasi Kitap sinifi altinda olusturulacak nesnelerinin propertieslerini olusturuyoruz..
+        {
+            get
+                {
+                    return  _bookName; // Burada herhangi bir sekilde Name propertiesin kullanilmasi durumunda okunacak deger _bookName degeri olacaktir.
+                }
+            set
+                {
+                    _bookName = value; // Burada kullanicinin yazmis oldugu deger _bookName degerine islenecektir.
+                }
+        }
+        public string WriterName
+        {
+            get { return _writerName; }
+            set { _writerName = value; }
+        }
+        public string WriterSurname
+        {
+            get
+            {
+                return _writerSurname;
+            }
+            set
+            {
+                _writerSurname = value;
+            }
+
+        }
+        public int PageNumber
+        {
+            get { return _pageNumber; }
+            set { _pageNumber = value; }
+        }
+        public string PublisherName
+        {
+            get { return _publisherName; }
+            set { _publisherName = value; }
+        }
+        public DateTime SignTime
+        {
+            get { return _signTime; }
+            set { _signTime = Convert.ToDateTime(value); }
+        }
+        
+    }
+}
---
+++ WEEK4/PatikaOOPLibraryExercise/PatikaOOPLibraryExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿//Ayşe Kulin'in 398 sayfalık, Remzi Kitabevi yayınevinden çıkan Adı Aylin kitabını oluşturunuz.
+
+//2 Adet constructor alternatifi olsun.
+
+//Bir tanesi default constructor tarzında parmetre almadan çalışan bir metot.
+
+//Diğeri Kitap adı, Yazar Adı, Sayfa Sayısı, Yayınevi  bilgilerini alıp bu değerleri nesne üretim aşamasında atayan bir metot.
+
+//KayıtTarihi nesne oluşturulunca otomatik olarak o an olarak atansın. (Her iki constructor için de geçerli)
+
+//Kodlarınızın altına birer yorum satırıyla örnek üzerinden Class , Property, New, Constructor kavramlarını açıklayınız.
+
+using PatikaOOPLibraryExercise;
+using System;
+
+namespace PatikaWeek4.OOP.FirstLibrary_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap1 = new Kitap(); // Burada kitap1 adinda nesne olusturmaktayiz..
+            kitap1.Name = "Adi Aylin";
+            kitap1.WriterName = "Ayse";
+            kitap1.WriterSurname = "Kulin";
+            kitap1.PageNumber = 398;
+            kitap1.PublisherName = "Remzi Kitabevi";
+
+            Kitap kitap2 = new Kitap("Henuz 17 Yasinda", "Ahmet Mithat Efendi", 236, "Turkiye Is Bankasi Kultur Sanat" );
+            Console.WriteLine($"Kitap Adi:{kitap1.Name}\n" +
+                $"Kitap Yazari Adi:{kitap1.WriterName}\n" +
+                $"Kitabin Yazarinin Soyadi:{kitap1.WriterSurname}\n" +
+                $"Kitabin Sayfa Sayisi: {kitap1.PageNumber}\n" +
+                $"Kitap Yayincisinin Adi:{kitap1.PublisherName}\n" +
+                $"Kitabin Yayinlanma Tarihi:{kitap1.SignTime}\n");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine($"Kitabin Adi:{kitap2.Name}\n" +
+                $"Kitabin Yazarinin Adi:{kitap2.WriterName}\n" +
+                $"Kitabin Sayfa sayisi:{kitap2.PageNumber}\n" +
+                $"Kitabin Yayinevi:{kitap2.PublisherName}\n" +
+                $"Kitabin Yayinlanma Tarihi:{kitap2.SignTime}\n");
+        }
+    }
+}

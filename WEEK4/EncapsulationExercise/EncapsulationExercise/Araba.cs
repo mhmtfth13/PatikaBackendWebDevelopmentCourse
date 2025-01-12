00aebbad40a91d2731b6b5c25c95ec1b94e005bc
mhmtfth13 <mhmtfthaydin@gmail.com>
2025-01-12 18:04:43 +0300

    Added Files

---
+++ WEEK4/EncapsulationExercise/EncapsulationExercise/Araba.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExercise
+{
+    internal class Araba
+    {
+        // marka, model, renk, kapi sayisi properties.
+
+        private string _marka; 
+        private string _model;
+        private string _renk;
+        private int _kapiSayisi;
+
+
+
+        public Araba(string marka , string model, string renk, int kapiSayisi)
+        {
+            _marka = marka;
+            _model = model;
+            _renk = renk;
+            KapiSayisi = kapiSayisi ;
+
+
+            Console.WriteLine($"Arabanin Markasi {marka}\n" +
+                $"Arabanin modeli {model}\n" +
+                $"Arabanin rengi {renk}\n" +
+                $"Arabinin kapi sayisi {KapiSayisi}");
+        }
+        public string Marka
+        {
+            get { return _marka; }
+            set { _marka = value; }
+
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public string Renk
+        {
+            get { return _renk; }
+            set { _renk = value; }
+        }
+        public int KapiSayisi
+        {
+            get { return _kapiSayisi; }
+            set
+            {
+                if (value != 2 && value != 4)
+                {
+                    Console.WriteLine("Uyarı: Kapı sayısı yalnızca 2 veya 4 olabilir. Kapı sayısı -1 olarak atanıyor.");
+                    _kapiSayisi = -1;
+                }
+                else 
+                {
+                    _kapiSayisi = value;
+                }
+
+            }
+        }
+    }
+}
---
+++ WEEK4/EncapsulationExercise/EncapsulationExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using EncapsulationExercise;
+using System;
+namespace  PatikaWeek4.EncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba("Bugatti","Veyron","Siyah",3);
+        }
+    }
+
+}

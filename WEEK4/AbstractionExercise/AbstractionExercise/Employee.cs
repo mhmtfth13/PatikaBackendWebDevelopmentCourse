ad71ce7f26ec1ef51fdab048dec0f29ef7fa64ef
Mehmet Fatih Aydın <116929673+mhmtfth13@users.noreply.github.com>
2025-01-14 19:50:20 +0300

    added src files

---
+++ WEEK4/AbstractionExercise/AbstractionExercise/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionExercise
+{
+    public abstract class Employee
+    {
+
+        private string _name;
+        private string _surname;
+        private string _department;
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+        public string Department
+        {
+            get { return _department; }
+            set { _department = value; }
+        }
+        public abstract void Gorev(string name, string surname, string department);
+        
+    }
+    public class SoftwareDeveloper : Employee
+    {
+        public override void Gorev(string name, string surname, string department)
+        {
+            Name= name;
+            Surname= surname;
+            Department= department;
+            Console.WriteLine($"{name} {surname} : working as a Software Developer");
+        }
+    }
+    public class ProjectLead : Employee
+    {
+        public override void Gorev(string name, string surname, string department)
+        {
+            Name= name;
+            Surname= surname;
+            Department = department;
+            Console.WriteLine($"{name} {surname} : working as a Project Leader");
+        }
+    }
+    public class SalesRepresentative : Employee
+    {
+        public override void Gorev(string name, string surname, string department)
+        {
+            Name= name;
+            Surname = surname;
+            Department= department;
+            Console.WriteLine($"{Name} {Surname} : working as a Sales Representative");
+        }
+    }
+}
---
+++ WEEK4/AbstractionExercise/AbstractionExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using AbstractionExercise;
+using System;
+
+namespace PatikaWeek4.AbstractExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee eleman1 = new ProjectLead();
+            eleman1.Gorev("Mehmet Fatih", "Aydin", "IT");
+            Employee eleman2 = new SoftwareDeveloper();
+            eleman2.Gorev("Mehmet Fatih", "Aydin", "Software Department");
+            Employee eleman3 = new SalesRepresentative();
+            eleman3.Gorev("Mehmet Fatih", "Aydin", "Sales");
+        }
+    }
+}

386e02cdb0e7fdfdbfab51fedbef9b5a94289f62
Mehmet Fatih Aydın <116929673+mhmtfth13@users.noreply.github.com>
2025-01-20 06:38:34 +0300

    adding src files

---
+++ WEEK5/ListExercise/ListExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PatikaWeek5.ListExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> davetliler = new List<string>();
+            string[] isimler = new string[] {
+                "Bülent Ersoy",
+                "Ajda Pekkan",
+                "Ebru Gündeş",
+                "BOS INSAN",
+                "Hande Yener",
+                "Tarkan",
+                "Funda Arar",
+                "Demet Akalın" };
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                davetliler.Add(isimler[i]);
+                Console.WriteLine($"{i+1} - {isimler[i]}");
+            }
+        }
+    }
+}

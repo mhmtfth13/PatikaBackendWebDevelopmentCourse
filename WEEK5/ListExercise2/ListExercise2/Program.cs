4b0f2f3b313b5459e792f85182040a1463befe48
Mehmet Fatih Aydın <116929673+mhmtfth13@users.noreply.github.com>
2025-01-20 06:53:56 +0300

    Added src files

---
+++ WEEK5/ListExercise2/ListExercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PatikaWeek5.ListExercise2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> kahveGirdileri = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write($"Kahve {i + 1}: ");
+                Console.ForegroundColor = ConsoleColor.Green;    // Kahve ismi için yeşil
+                string kahveAdi = Console.ReadLine();
+                kahveGirdileri.Add(kahveAdi);
+            }
+            Console.WriteLine();  // Boş satır
+            Console.ForegroundColor = ConsoleColor.White;  // Başlık için beyaz
+            Console.WriteLine("Girilen Kahve İsimleri:");
+            foreach (string kahve in kahveGirdileri)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;  // Kahve isimleri için gri
+                Console.WriteLine(kahve);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}

65df51b329cb709fa337cf6c1ead2bf569b75f75
Mehmet Fatih Aydın <116929673+mhmtfth13@users.noreply.github.com>
2025-01-21 06:55:19 +0300

    added src files

---
+++ WEEK5/TryCatchExercise/TryCatchExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatikaWeek5.TryCatchExercise
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Lutfen bir sayi giriniz: ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"{x} sayisinin karesi = "+ SayiGir(x));
+
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Gecersiz giris! Lutfen sayi giriniz!");
+                    continue;
+                }
+            }
+
+        }
+        public static int SayiGir(int x )
+        {
+            return x * x;
+        }
+    }
+}

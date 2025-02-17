58fa98b44401a89d041127298675ad69698fbd22
Mehmet Fatih Aydın <116929673+mhmtfth13@users.noreply.github.com>
2025-02-17 05:42:30 +0300

    added files

---
+++ WEEK8/Basic_MVC_Exercies_Solution/Basic_MVC_Exercies_Solution/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basic_MVC_Exercies_Solution.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEEK8/Basic_MVC_Exercies_Solution/Basic_MVC_Exercies_Solution/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basic_MVC_Exercies_Solution.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
